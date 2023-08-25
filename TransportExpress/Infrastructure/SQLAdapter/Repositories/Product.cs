a41a22c8970a6793bb6e80388a68ad2a37eea342
Kevin Baquero <baquerochavarro@gmail.com>
2023-08-25 15:41:30 -0500

    Endpoints of Product entity
    GetProducts

---
+++ TransportExpress/Infrastructure/SQLAdapter/Repositories/Product.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using TransportExpress.Domain.Common;
+using TransportExpress.Infrastructure.SQLAdapter.Gateway;
+using TransportExpress.UseCases.IRepositories;
+
+namespace TransportExpress.Infrastructure.SQLAdapter.Repositories
+{
+    public class Product : IProduct
+    {
+        private readonly IDbConnectionBuilder _dbConnectionBuilder;
+        private readonly string _tableNameProduct = "Product";
+
+        public Product(IDbConnectionBuilder dbConnectionBuilder)
+        {
+            _dbConnectionBuilder = dbConnectionBuilder;
+        }
+
+        public async Task<List<Domain.Entities.Product>> GetProductsAsync()
+        {
+            var connection = await _dbConnectionBuilder.CreateConnectionAsync();
+            string query = $"SELECT * FROM {_tableNameProduct}";
+            var productsFound = (from product in await connection.QueryAsync<Domain.Entities.Product>(query)
+                                 where product.StateProduct == Enums.StateEntity.Active
+                                 select product).ToList();
+            connection.Close();
+            return productsFound;
+        }
+    }
+}
